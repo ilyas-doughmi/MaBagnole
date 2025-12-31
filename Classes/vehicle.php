5b0d46c7df9453271e08bfb9e0d76d10d8bbfa67
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-31 12:37:36 +0100

    feat: add vehicle class with properties for vehicle management

---
+++ Classes/vehicle.php
@@ -0,0 +1,12 @@
+<?php
+
+class vehicle{
+    private $vehicle_id;
+    private $brand;
+    private $model;
+    private $pricePerDay;
+    private $description;
+    private $imagePath;
+    private $isAvailable;
+    private $createdAt;
+}
