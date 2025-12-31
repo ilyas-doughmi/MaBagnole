0b3c29bedbf15d981015a7a75dd33d0d3ad770b9
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-31 11:11:11 +0100

    feat: create user class with properties for user management

---
+++ Classes/user.php
@@ -0,0 +1,13 @@
+<?php
+
+class user{
+    protected $userId;
+    protected $fullname;
+    protected $email;
+    protected $password;
+    protected $role;
+    protected $createdAt;
+    protected $isActive;
+
+    
+}
