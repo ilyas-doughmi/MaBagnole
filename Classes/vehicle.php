0a1b113798dddb01f1b4b74624880e07a3344c5c
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-31 12:54:08 +0100

    feat: add getVehicles method to retrieve vehicle data from the database

--- Classes/vehicle.php
+++ Classes/vehicle.php
@@ -27,4 +27,20 @@ class vehicle{
         return $this->$name;
     }
 
+    public function getVehicles()
+    {
+        $query = "SELECT v.*, c.category_name 
+                  FROM vehicle v 
+                  LEFT JOIN category c ON v.category_id = c.category_id";
+        $stmt = $this->pdo->query($query);
+        $stmt->execute();
+        $vehicles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $count = count($vehicles);
+
+        return [
+            "vehicles" => $vehicles,
+            "count" => $count
+        ];
+    }
+
 }
