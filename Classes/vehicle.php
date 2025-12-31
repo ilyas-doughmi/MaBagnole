7c97b406dadfc691cea8055613a4a7ff8dda1721
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-31 12:38:10 +0100

    feat: add magic methods __set and __get to vehicle class for property access

--- Classes/vehicle.php
+++ Classes/vehicle.php
@@ -9,4 +9,14 @@ class vehicle{
     private $imagePath;
     private $isAvailable;
     private $createdAt;
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
 }
